93d3b82808bf6f04c0f7755ef6052d78ff9fb9d2
Bruno Santana <bsantana.it@gmail.com>
2023-01-09 01:02:24 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -451,19 +451,13 @@ App = function (App, $, window, document) {
         var $filter_project = $('.project');
         var $filter_tiger = $('.project-filter li');
         
-        $filter_project.each(function(){
-            var $self = $(this),
-                f_layout =($self.data('layout')) ? $self.data('layout') : 'packed';
-            $(this).filterizr({
-                layout: f_layout,
-            });
-        })
+       
         
         $filter_tiger.on('click',function() { 
             $filter_tiger.removeClass('active');
             $(this).addClass('active');
         });
-        setTimeout(() => {
+
         $.getJSON( "enterprises.json", function(data) {
             $.each(data.enterprises, function( k, v ) {
 
@@ -534,7 +528,14 @@ App = function (App, $, window, document) {
                         
             });
         });
-    }, "3000")
+            
+        $filter_project.each(function(){
+            var $self = $(this),
+                f_layout =($self.data('layout')) ? $self.data('layout') : 'packed';
+            $(this).filterizr({
+                layout: f_layout,
+            });
+        })
 
 
 	};
